672a785bca3d64243bfae3d9be4714f9542b595c
Yuta Fukuchi <fufufu10155@gmail.com>
2020-05-30 20:49:05 +0900

    sortの三項演算子

---
+++ js.js
@@ -0,0 +1,8 @@
+// const cards = [20, 5, 4]
+// const a = cards.sort((a, b) => a-b)
+// console.log(a)
+
+args = ["3","5","7","9"]
+const N = args[0],
+nums = args.slice(1, args.length-1 );
+console.log(nums)
